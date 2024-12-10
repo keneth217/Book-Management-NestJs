163a424716a74c539df6181c4028dda92397a4ce
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-10 22:11:22 +0300

    correct login payload

--- src/auth/local-auth.guard.ts
+++ src/auth/local-auth.guard.ts
@@ -1,5 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class LocalAuthGuard extends AuthGuard('local') {}
+export class LocalAuthGuard extends AuthGuard('local') {
+
+  canActivate(context: ExecutionContext) {
+    console.log('LocalAuthGuard activated');
+    return super.canActivate(context);
+  }
+}
