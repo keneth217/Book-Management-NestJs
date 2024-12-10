e88f894613bf4c91879e87b6297e5f66b801bb76
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-10 21:52:31 +0300

    crud for books and users ,jwt auth

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { UsersModule } from '../users/users.module';
+import { LocalStrategy } from './local.strategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constants';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  providers: [AuthService, LocalStrategy, JwtStrategy],
+  exports: [AuthService],
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+})
+export class AuthModule {}
