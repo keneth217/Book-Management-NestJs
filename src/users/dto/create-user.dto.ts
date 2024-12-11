1147b6cdf09a2290a56790dc4abab99d2bb744ae
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-11 16:54:37 +0300

    correct login payload and requests

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,20 @@
+import { IsEmail, IsNotEmpty, IsOptional, Length } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty()
   name: string;
+
+  @IsNotEmpty()
+  @Length(4, 20)
   userName: string;
+
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @Length(6, 50)
   password: string;
-  phone: string;
+
+  @IsOptional()
+  phone?: string;
 }
