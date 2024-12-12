b55fd03859ccc7f0e9b93a7e3bba7acec00dcb79
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-12 17:29:37 +0300

    Role-based access control (RBAC) and rate limiting

--- src/auth/roles/roles.guard.ts
+++ src/auth/roles/roles.guard.ts
@@ -1,21 +1,46 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+// src/auth/roles/roles.guard.ts
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+  Logger,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from './roles.decorator';
 import { Role } from './role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private reflector: Reflector) {}
+  private readonly logger = new Logger(RolesGuard.name);
+
+  constructor(private readonly reflector: Reflector) {}
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>(ROLES_KEY, [
       context.getHandler(),
       context.getClass(),
     ]);
+
     if (!requiredRoles) {
       return true;
     }
+
     const { user } = context.switchToHttp().getRequest();
-    return requiredRoles.some((role) => user.roles?.includes(role));
+    console.log('User roles:', user?.roles);
+    console.log('Required roles:', requiredRoles);
+    const userRoles = user?.roles || [Role.Guest];
+
+    const hasRole = requiredRoles.some((role) => userRoles.includes(role));
+
+    if (!hasRole) {
+      this.logger.warn(
+        `Access denied. User roles: ${userRoles}, required roles: ${requiredRoles}`,
+      );
+      throw new ForbiddenException(
+        'You do not have the required permissions to access this resource.',
+      );
+    }
+    return hasRole;
   }
 }
