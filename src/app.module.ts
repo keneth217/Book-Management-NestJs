958e5f580f01d2df456ad689542368601d97f014
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-13 21:45:43 +0300

    Role-based access control,book controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { BooksModule } from './books/books.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import * as Joi from '@hapi/joi';
 import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { BooksModule } from './books/books.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { APP_GUARD } from '@nestjs/core';
+import { RolesModule } from './roles/roles.module';
+
 @Module({
   imports: [
     DevtoolsModule.register({
@@ -19,27 +22,42 @@ import { APP_GUARD } from '@nestjs/core';
         limit: 10,
       },
     ]),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      username: 'keneth',
-      password: 'korir',
-      port: 5432,
-      database: 'book-management',
-      synchronize: true,
-      entities: ['dist/**/*.entity{.ts,.js}'],
+    // Initialize ConfigModule and validate .env variables
+    ConfigModule.forRoot({
+      isGlobal: true, // Makes config available globally
+      validationSchema: Joi.object({
+        DATABASE_HOST: Joi.string().required(),
+        DATABASE_PORT: Joi.number().required(),
+        DATABASE_USER: Joi.string().required(),
+        DATABASE_PASSWORD: Joi.string().required(),
+        DATABASE_NAME: Joi.string().required(),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+      }),
+    }),
+
+    // Configure TypeORM using validated .env variables
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USER'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        synchronize: true,
+        entities: ['dist/**/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
     }),
     BooksModule,
     UsersModule,
     AuthModule,
+    RolesModule,
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: APP_GUARD,
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule {}
