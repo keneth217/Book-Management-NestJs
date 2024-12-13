d39222c930b3a5058b108133996b095be3f862f1
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-13 21:46:13 +0300

    Role-based access control,book controller

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,12 @@ async function bootstrap() {
 
   // Optionally enable CORS if needed
   app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
