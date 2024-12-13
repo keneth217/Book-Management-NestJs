aa364291a76b27e1045c1ce086beadc080addb55
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-13 21:46:10 +0300

    Role-based access control,book controller

--- src/auth/roles/ChangeRoleDto.ts
+++ src/auth/roles/ChangeRoleDto.ts
@@ -1,9 +1,8 @@
 // dto/change-role.dto.ts
-import { IsEnum, IsNotEmpty } from 'class-validator';
-import { Role } from './role.enum';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ChangeRoleDto {
   @IsNotEmpty()
-  @IsEnum(Role, { message: 'Role must be a valid value (Admin, User, etc.)' })
-  role: Role;
+  @IsString()
+  role: string; // Accept role as a string
 }
