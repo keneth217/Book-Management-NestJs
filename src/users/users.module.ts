09d70d2e9a6573bb31755f37ffa3b1a8e93eece8
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-13 21:46:39 +0300

    Role-based access control,book controller

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,15 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { RolesModule } from '../roles/roles.module';
+import { Role } from '../roles/entities/role.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User, Role]), // Ensure UserRepository and RoleRepository are available
+    RolesModule, // Import the RolesModule so RoleRepository is accessible
+  ],
+
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
