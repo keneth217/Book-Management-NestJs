92b42aa14ceea72059d11ed4da3a7c8eec8dc4a0
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-14 22:55:37 +0300

    implement google oauth20

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as session from 'express-session';
+import passport from 'passport';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -11,6 +13,18 @@ async function bootstrap() {
 
   // Optionally enable CORS if needed
   app.enableCors();
+  app.use(
+    session({
+      secret: 'kefefewfhg4rhgienv',
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        maxAge: 60000,
+      },
+    }),
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
