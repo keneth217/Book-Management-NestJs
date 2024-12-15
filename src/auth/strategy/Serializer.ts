e41760fcb5f5f67c703668ebcb5061ae6a0de66e
keneth/217 <kipyegonkeneth03@gmail.com>
2024-12-15 13:56:17 +0300

    oauth two validation,redirect response

--- src/auth/strategy/Serializer.ts
+++ src/auth/strategy/Serializer.ts
@@ -1,20 +1,21 @@
 import { PassportSerializer } from '@nestjs/passport';
-import { Inject } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { AuthService } from '../auth.service';
-
+import { User } from '../../users/entities/user.entity';
+@Injectable()
 export class SessionSerializer extends PassportSerializer {
-  constructor(
-    @Inject('AUTH_SERVICE')
-    private readonly authService: AuthService,
-  ) {
+  constructor(private readonly authService: AuthService) {
     super();
   }
 
-  deserializeUser(payload: any, done: Function): any {
-
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  async deserializeUser(payload: User, done: Function): Promise<any> {
+    const user = await this.authService.finduser(payload.id);
+    return user ? done(null, user) : done(null, null);
   }
 
-  serializeUser(user: any, done: Function): any {
-
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
+  serializeUser(user: User, done: Function): any {
+    done(null, user);
   }
 }
